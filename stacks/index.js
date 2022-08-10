e66dbc775b25916cb47cb710009ae0a6d3990d5d
Jorge Puente <jorge.puente.08@gmail.com>
2022-08-10 15:11:03 -0400

    Configurando nuestra aplicación React con API

--- stacks/index.js
+++ stacks/index.js
@@ -1,6 +1,7 @@
 import { StorageStack } from "./StorageStack";
 import { ApiStack } from "./ApiStack";
 import { AuthStack } from "./AuthStack";
+import { FrontendStack } from "./FrontendStack";
 
 export default function main(app) {
   app.setDefaultFunctionProps({
@@ -10,7 +11,7 @@ export default function main(app) {
       format: "esm",
     },
   });
-  app.stack(StorageStack).stack(ApiStack).stack(AuthStack);
+  app.stack(StorageStack).stack(ApiStack).stack(AuthStack).stack(FrontendStack);
 }
 
 /* export default function main(app) {
